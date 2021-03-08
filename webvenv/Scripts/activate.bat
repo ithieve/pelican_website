4b5c4ba362d2c19d9f64223d4c501d0eaeb3ca87
ithieve <ithieve@yahoo.com>
2021-03-08 13:32:23 +0600

    fix venv

--- webvenv/Scripts/activate.bat
+++ webvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=P:\Ultimate Workspace\Programing\PythonWebDev\webvenv
+set VIRTUAL_ENV=P:\Ultimate Workspace\Programing\PythonWebDev\website\webvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
